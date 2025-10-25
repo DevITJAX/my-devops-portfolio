974f628346b8afb278486d2d7971f47a15017b91
DevITJAX <devitjax@users.noreply.github.com>
2025-10-25 16:35:46 +0100

    Move scroll indicator arrow down on hero section

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -142,7 +142,7 @@ const Hero = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 1.5 }}
-          className="absolute bottom-8 left-1/2 transform -translate-x-1/2"
+          className="absolute bottom-2 left-1/2 transform -translate-x-1/2"
         >
           <motion.button
             onClick={scrollToAbout}
